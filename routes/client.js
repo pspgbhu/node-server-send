de311329a92246e6078e44142028d25957c7ea5e
hucong <hucong@jd.com>
2018-01-02 10:54:20 +0800

    fix: fix some bug

--- routes/client.js
+++ routes/client.js
@@ -8,7 +8,7 @@ const sse = require('../libs/sse');
 router
   .get('/test', async (ctx) => {
     // otherwise node will automatically close this connection in 10 minutes.
-    ctx.req.setTimeout(1000 * 60, 10);
+    ctx.req.setTimeout(1000 * 60 * 10);
     ctx.type = 'text/event-stream; charset=utf-8';
     ctx.set('Cache-Control', 'no-cache');
     ctx.set('Connection', 'keep-alive');
