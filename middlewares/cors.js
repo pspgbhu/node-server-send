67ba3321511505e38e637c636bbe4060050a6a7e
hucong <hucong@jd.com>
2018-01-03 15:31:17 +0800

    fix: cors middleware 修复

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 module.exports = () => async (ctx, next) => {
   ctx.set({
-    'Access-Control-Allow-Origin': ctx.request.header.origin,
+    'Access-Control-Allow-Origin': ctx.origin,
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
     'Access-Control-Allow-Credentials': true,
     'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
